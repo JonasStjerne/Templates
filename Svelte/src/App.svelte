9b2cf3c8854c254927e6ee2181eb61ec97ec9247
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2022-03-07 09:29:56 +0100

    Stripe

--- Svelte/src/App.svelte
+++ Svelte/src/App.svelte
@@ -1,5 +1,20 @@
 <script>
-	import Dawa from './DAWA.svelte';
-</script>
+	import { Router, Link, Route } from "svelte-routing";
+	import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+	
+	import Stripe from './Stripe/Products.svelte';
+	import Dawa from './DAWA.svelte'
+	
 
-<Dawa></Dawa>
+	export let url = "";
+</script>
+<Router url="{url}">
+	<nav class="d-flex flex-row-reverse text-primary" style="gap: 2rem;">
+	  <Link to="dawa">Address Autocomplete</Link>
+	  <Link to="stripe">Stripe integration</Link>
+	</nav>
+	<div>
+	  <Route path="dawa" component="{Dawa}" />
+	  <Route path="stripe" component="{Stripe}" />
+	</div>
+</Router>
---
+++ Svelte/src/Stripe/Products.svelte
@@ -0,0 +1,63 @@
+<script>
+      const products = [
+            {
+                  img : "https://i.imgur.com/EHyR2nP.png",
+                  title : "Product 1",
+                  price : 20.00
+            },
+            {
+                  img : "https://i.imgur.com/EHyR2nP.png",
+                  title : "Product 2",
+                  price : 10.00
+            },
+            {
+                  img : "https://i.imgur.com/EHyR2nP.png",
+                  title : "Product 3",
+                  price : 5.00
+            },
+            {
+                  img : "https://i.imgur.com/EHyR2nP.png",
+                  title : "Product 4",
+                  price : 2.99
+            }
+
+      ]
+</script>
+<div class="productContainer py-5">
+      {#each products  as product}
+            <div class="product">
+                  <img src={product.img} alt="Product" class="img-fluid"/>
+                  <div class="d-flex justify-content-between align-items-center">
+                        <div class="description">
+                              <h3>{product.title}</h3>
+                              <h5>${product.price}</h5>
+                        </div>
+                        <form action="/create-checkout-session" method="POST">
+                              <button type="submit" id="checkout-button">Checkout</button>
+                        </form>
+                  </div>
+            </div>
+      {/each}
+</div>
+
+<style>
+
+      .productContainer {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            width: 100%;
+            height: auto;
+            gap: 4rem;
+            justify-content: center;
+            align-items: center;
+            
+      }
+
+      .product {
+            width: 30%;
+      }
+
+
+
+</style>
